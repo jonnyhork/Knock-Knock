5dc2bfe3b500f3d7af98cbcc532d6d0386cc78a1
Jonny Hork <JonnyHork@gmail.com>
2017-01-19 10:14:56 -0700

    First project Using JS to interact with HTML
    Simple knock knock Joke

---
+++ main.js
@@ -0,0 +1,24 @@
+// Part 1 of the Joke
+
+// document.getElementById("button-part1").classList.add("show");
+
+
+
+
+// Part 2 of the Joke
+
+document.getElementById("button-part2").classList.add("hide");
+document.getElementById("button-part1").addEventListener("click", function(){
+
+document.getElementById("button-part2").classList.add("show");
+});
+
+
+//Part 3 of the Joke
+
+document.getElementById("joke-part3").classList.add("hide");
+document.getElementById("button-part2").addEventListener("click",function(){
+
+  document.getElementById("joke-part3").classList.add("show");
+
+});
