b881fbe1a21088552b1b9cf68a28bcef704e6757
Jonny Hork <JonnyHork@gmail.com>
2017-07-29 13:42:41 -0600

    fixed the flow of the joke for part two

--- main.js
+++ main.js
@@ -8,16 +8,18 @@
 // Part 2 of the Joke
 
 document.getElementById("button-part2").classList.add("hide");
-document.getElementById("button-part1").addEventListener("click", function(){
+document.getElementById("joke-part2").classList.add("hide");
+document.getElementById("button-part1").addEventListener("click", function() {
 
-document.getElementById("button-part2").classList.add("show");
+  document.getElementById("joke-part2").classList.add("show");
+  document.getElementById("button-part2").classList.add("show");
 });
 
 
 //Part 3 of the Joke
 
 document.getElementById("joke-part3").classList.add("hide");
-document.getElementById("button-part2").addEventListener("click",function(){
+document.getElementById("button-part2").addEventListener("click", function() {
 
   document.getElementById("joke-part3").classList.add("show");
 
